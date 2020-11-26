030c542ae5a4b257ff2ec11bdc398e7377055268
Ruan-Pereira <ruanborbapereira@gmail.com>
2020-11-25 23:03:39 -0300

    Criando menu superior

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ function App() {
   return (
     <div>
       <Menu   />
-      <Banner />
+      {/* <Banner />
       <Carrousel />
       <Formulario   />
-      <Footer /> 
+      <Footer />  */}
    </div>
   );
 }
--- src/components/menu_top/index.js
+++ src/components/menu_top/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import './style.css'
+import './style.css';
+import Logo from "../../images/Logo_Corebiz.png"
+import Carrinho from "../../images/Compras.png"
+import Search from "../../images/Search.png"
+
 
 export default () => {
     return (
        <div>
-        <h1>menu_top</h1>
+           <div className="menu_superior_mobile">
+                <div className="menu_mobile">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <img className="logo" src={Logo} />
+                <img className="carrinho" src={Carrinho}/>
+           </div>
+           <div className="pesquisar">
+                <input placeholder="O que você esta procurando?"/>     
+                 <img src={Search} />
+           </div>
        </div>
     );
 }
