5e54545dcbda43507dd53ed3ba48d91960a91156
Ruan-Pereira <ruanborbapereira@gmail.com>
2020-11-29 00:01:01 -0300

    Estilizando o carrousel

--- src/components/carrousel/index.js
+++ src/components/carrousel/index.js
@@ -1,46 +1,99 @@
 import React, {useEffect, useState} from "react";
 import Slider from "react-slick";
 import Api from './Api'
+import './style.css'
+/* import Positivo from '../../images/Positivo.png'
+import Negativo from '../../images/Negativo.png'  */
 
 
 export default () => {
   
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 2,
-    slidesToScroll:2
-  };
-
-const [teste, setProdutos ] = useState([]);
-
+  const [teste, setProdutos ] = useState([]);
+  
 useEffect(() => {
   const loadproducts = async() => {
     
     let list = await Api.getprodutos();
     setProdutos(list[0].Array_products)
-    
-
+  
   }
   loadproducts();
-
-
 }, []);
 
-  
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll:4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+    
+      
+    ]
+  };
+
+let variavel = '';
+
+/* const starRating = (item) => {
+  let i = 1
+  for (i; i <= 5; i++) {
+    if(item > i) {
+      return(
+        console.log(item)
+      )
+    } else {
+     return(
+      <img src={Negativo}/>
+     )
+    }
+}
+ 
+} */
   return(
+    
     <Slider {...settings}>
-   
+  
        {teste.map ((item, key) =>(
-          <div>
-             <img src={`${item.imageUrl}`} alt="happy" />
-             {item.productName}
-             <h1>por <b>{(item.price / 100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL'})}</b></h1>
+          <div class="carrousel">
+             <img className="Image_produto" src={`${item.imageUrl}`} alt="happy" />
+             
+       <h3>{item.productName}</h3><br/> 
+
+       {/* { starRating(item.stars) }  */}
+
+            
+             <h2>{item.listPrice  !== null ? (
+               (item.listPrice / 100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL'})
+             ):( <div/>
+             )}</h2>
+
+             <h4>por <b>{(item.price / 100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL'})}</b></h4>
+
+              <p> {( 
+                variavel = item.installments[0] && item.installments[0]  ? (
+                  ' ou em  ' +
+                  item.installments[0].quantity +
+
+                  'x de ' +
+                      (item.installments[0].value / 100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL'})
+                  ) : (
+                    <div className="instalments" />
+                  )
+                )} </p>
+
+                <buttom className="Comprar"> Comprar </buttom>
           </div>
          ))} 
-         
-    
+        
     </Slider>
   )
 };
--- src/components/formulario/index.js
+++ src/components/formulario/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css'
 
 export default () => {
+
+    const [search, setSearch] = useState('');
+
+    function handleSubmit(event){
+        event.preventDefault();
+
+        console.log(event)
+    }
     return (
        <div className="area_total_formulario">
            <div className="area_formulario">
                 <h2>Participe de nossas news com promoções e novidades!</h2>
-                <input placeholder="Digite seu nome"></input>
-                <input placeholder="Digite seu email"></input>
-                <botton className="receber_novidades">
-                       <h4>Eu quero!</h4>
-                </botton>
+               
+               <form   class="Notificações_Corebiz">
+                <input      className="input"
+                            placeholder="Digite seu nome"
+                            value={search}
+                            type="text"
+                            name="name"
+                            data-rules="required"
+                            onChange={e => setSearch(e.currentTarget.value)}
+                            />
+
+                    <input className="input"
+                            placeholder="Digite seu nome"
+                            type="text"
+                            name="email"
+                            data-rules="required"/>
+
+                    <label className="botão">
+                         <input className="receber_novidades" type="submit" value="Eu quero" onChange={handleSubmit}/>
+                                             
+                    </label>
+               </form>
            </div>
+           
        </div>
     );
 }
