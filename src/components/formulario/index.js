0db95774e4bc727da2d2f612b2fb4857c9e84749
Ruan-Pereira <ruanborbapereira@gmail.com>
2020-11-30 00:31:02 -0300

    Projeto final

--- src/components/formulario/index.js
+++ src/components/formulario/index.js
@@ -1,34 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './style.css'
 
 export default () => {
-
+   
 
     return (
        <div className="area_total_formulario">
            <div className="area_formulario">
                 <h2>Participe de nossas news com promoções e novidades!</h2>
                
-               <form id="formulario" name="" method="" action="" class="Notificações_Corebiz">
-                <input     id="nome"
-                            type="text"
-                            name="nome"
-                            className="input"
-                            placeholder="Digite seu nome"
-                            />
-                    <input className="input"
-                             id="email"
-                            placeholder="Digite seu nome"
-                            type="text"
-                            name="email"
-                            />
-                    <label className="botão">
-                         <button className="receber_novidades" type="submit" value="Eu quero" >Eu quero!</button>
-                                             
-                    </label>
+               <form  id="formulario" name="" method="" action="" class="Notificações_Corebiz">
+               <label>
+                    <input className="input"placeholder="Digite seu nome" />
+               </label>
+
+                <label>
+                         <input className="input" placeholder="Digite seu nome"/>
+                 </label>
+                <label
+                 className="botão">
+                         <button className="receber_novidades" type="submit"  >Eu quero!</button>                    
+                </label>
                </form>
            </div>
            
        </div>
-    );
-}
+        );
+    }
